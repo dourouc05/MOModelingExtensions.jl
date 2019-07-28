0628822f41d6daff7b3d6a78a9dcd27ec78417f9
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2019-07-28 21:37:16 +0200

    Implement minimum and maximum.

--- src/MOModelingExtensions.jl
+++ src/MOModelingExtensions.jl
@@ -1,7 +1,7 @@
 module MOModelingExtensions
 
 using JuMP
-import Base: max, min
+import Base: max, min, maximum, minimum
 
 function _check_var_has_bounds(var::AbstractVariableRef)
     if ! has_lower_bound(var)
@@ -36,6 +36,32 @@ function max(a::AbstractVariableRef, b::AbstractVariableRef)::AbstractVariableRe
     @constraint(model, x <= b + M * (1 - z))
     @constraint(model, x >= a)
     @constraint(model, x >= b)
+
+    return x
+end
+
+function maximum(vars::AbstractVector{<:AbstractVariableRef})::AbstractVariableRef
+    # Same model as max, but with one binary variable per input variable.
+    model = owner_model(vars[1])
+    for var in vars # TODO: Only check for first variable.
+        check_belongs_to_model(var, model)
+    end
+
+    for var in vars
+        _check_var_has_bounds(var)
+    end
+
+    x_lower_bound = maximum(lower_bound.(vars))
+    x_upper_bound = maximum(upper_bound.(vars))
+    # Maximum difference between two variables.
+    M = maximum(abs.(lower_bound(v1) - upper_bound(v2) for v1 in vars, v2 in vars))
+
+    x = @variable(model, lower_bound=x_lower_bound, upper_bound=x_upper_bound)
+    z = @variable(model, [eachindex(vars)], binary=true)
+
+    @constraint(model, [var in eachindex(vars)], x <= vars[var] + M * z[var])
+    @constraint(model, [var in eachindex(vars)], x >= vars[var])
+    @constraint(model, sum(z) == 1)
 
     return x
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,115 +3,187 @@ using Test, GLPK
 
 @testset "Modeling extensions" begin
     @testset "Maximum" begin
-        @testset "Both variables nonnegative" begin
-            m = Model(with_optimizer(GLPK.Optimizer))
-            @variable(m, 0 <= x <= 5)
-            @variable(m, 0 <= y <= 5)
-            @constraint(m, max(x, y) <= 4)
-            @objective(m, Max, x + y)
-            optimize!(m)
+        @testset "max" begin
+            @testset "Both variables nonnegative" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, 0 <= x <= 5)
+                @variable(m, 0 <= y <= 5)
+                @constraint(m, max(x, y) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
 
-            @test value(x) ≈ 4.0
-            @test value(y) ≈ 4.0
+                @test value(x) ≈ 4.0
+                @test value(y) ≈ 4.0
+            end
+
+            @testset "First variable nonpositive" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, -5 <= x <= 0)
+                @variable(m, 0 <= y <= 5)
+                @constraint(m, max(x, y) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
+
+                @test value(x) ≈ 0.0
+                @test value(y) ≈ 4.0
+            end
+
+            @testset "Second variable nonpositive" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, 0 <= x <= 5)
+                @variable(m, -5 <= y <= 0)
+                @constraint(m, max(x, y) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
+
+                @test value(x) ≈ 4.0
+                @test value(y) ≈ 0.0
+            end
+
+            @testset "Both variables mixed sign" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, -5 <= x <= 5)
+                @variable(m, -5 <= y <= 5)
+                @constraint(m, max(x, y) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
+
+                @test value(x) ≈ 4.0
+                @test value(y) ≈ 4.0
+            end
         end
 
-        @testset "First variable nonpositive" begin
-            m = Model(with_optimizer(GLPK.Optimizer))
-            @variable(m, -5 <= x <= 0)
-            @variable(m, 0 <= y <= 5)
-            @constraint(m, max(x, y) <= 4)
-            @objective(m, Max, x + y)
-            optimize!(m)
+        @testset "maximum" begin
+            @testset "maximum([x, y])" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, 0 <= x <= 5)
+                @variable(m, 0 <= y <= 5)
+                @constraint(m, maximum([x, y]) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
 
-            @test value(x) ≈ 0.0
-            @test value(y) ≈ 4.0
-        end
+                @test value(x) ≈ 4.0
+                @test value(y) ≈ 4.0
+            end
 
-        @testset "Second variable nonpositive" begin
-            m = Model(with_optimizer(GLPK.Optimizer))
-            @variable(m, 0 <= x <= 5)
-            @variable(m, -5 <= y <= 0)
-            @constraint(m, max(x, y) <= 4)
-            @objective(m, Max, x + y)
-            optimize!(m)
+            @testset "maximum([x, y, z])" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, 0 <= x <= 5)
+                @variable(m, 0 <= y <= 5)
+                @variable(m, 4 <= z <= 5)
+                @constraint(m, maximum([x, y, z]) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
 
-            @test value(x) ≈ 4.0
-            @test value(y) ≈ 0.0
-        end
-
-        @testset "Both variables mixed sign" begin
-            m = Model(with_optimizer(GLPK.Optimizer))
-            @variable(m, -5 <= x <= 5)
-            @variable(m, -5 <= y <= 5)
-            @constraint(m, max(x, y) <= 4)
-            @objective(m, Max, x + y)
-            optimize!(m)
-
-            @test value(x) ≈ 4.0
-            @test value(y) ≈ 4.0
+                @test value(x) ≈ 4.0
+                @test value(y) ≈ 4.0
+                @test value(y) ≈ 4.0
+            end
         end
     end
 
     @testset "Minimum" begin
-        @testset "Both variables nonnegative" begin
-            m = Model(with_optimizer(GLPK.Optimizer))
-            @variable(m, 0 <= x <= 5)
-            @variable(m, 0 <= y <= 5)
-            @constraint(m, min(x, y) <= 4)
-            @objective(m, Max, x + y)
-            optimize!(m)
+        @testset "min" begin
+            @testset "Both variables nonnegative" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, 0 <= x <= 5)
+                @variable(m, 0 <= y <= 5)
+                @constraint(m, min(x, y) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
 
-            println(termination_status(m))
+                if value(x) ≈ 4.0
+                    @test value(x) ≈ 4.0
+                    @test value(y) ≈ 5.0
+                else
+                    @test value(x) ≈ 5.0
+                    @test value(y) ≈ 4.0
+                end
+            end
 
-            # if value(x) ≈ 4.0
-            #     @test value(x) ≈ 4.0
-            #     @test value(y) ≈ 5.0
-            # else
-            #     @test value(x) ≈ 5.0
-            #     @test value(y) ≈ 4.0
-            # end
+            @testset "First variable nonpositive" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, -5 <= x <= 0)
+                @variable(m, 0 <= y <= 5)
+                @constraint(m, min(x, y) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
+
+                @test value(x) ≈ 0.0
+                @test value(y) ≈ 5.0
+            end
+
+            @testset "Second variable nonpositive" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, 0 <= x <= 5)
+                @variable(m, -5 <= y <= 0)
+                @constraint(m, min(x, y) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
+
+                @test value(x) ≈ 5.0
+                @test value(y) ≈ 0.0
+            end
+
+            @testset "Both variables mixed sign" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, -5 <= x <= 5)
+                @variable(m, -5 <= y <= 5)
+                @constraint(m, min(x, y) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
+
+                if value(x) ≈ 4.0
+                    @test value(x) ≈ 4.0
+                    @test value(y) ≈ 5.0
+                else
+                    @test value(x) ≈ 5.0
+                    @test value(y) ≈ 4.0
+                end
+            end
         end
 
-        @testset "First variable nonpositive" begin
-            m = Model(with_optimizer(GLPK.Optimizer))
-            @variable(m, -5 <= x <= 0)
-            @variable(m, 0 <= y <= 5)
-            @constraint(m, min(x, y) <= 4)
-            @objective(m, Max, x + y)
-            optimize!(m)
+        @testset "minimum" begin
+            @testset "minimum([x, y])" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, 0 <= x <= 5)
+                @variable(m, 0 <= y <= 5)
+                @constraint(m, minimum([x, y]) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
 
-            println(termination_status(m))
+                if value(x) ≈ 4.0
+                    @test value(x) ≈ 4.0
+                    @test value(y) ≈ 5.0
+                else
+                    @test value(x) ≈ 5.0
+                    @test value(y) ≈ 4.0
+                end
+            end
 
-            # @test value(x) ≈ 0.0
-            # @test value(y) ≈ 4.0
-        end
+            @testset "minimum([x, y, z])" begin
+                m = Model(with_optimizer(GLPK.Optimizer))
+                @variable(m, 0 <= x <= 5)
+                @variable(m, 0 <= y <= 5)
+                @variable(m, 4 <= z <= 5)
+                @constraint(m, minimum([x, y, z]) <= 4)
+                @objective(m, Max, x + y)
+                optimize!(m)
 
-        @testset "Second variable nonpositive" begin
-            m = Model(with_optimizer(GLPK.Optimizer))
-            @variable(m, 0 <= x <= 5)
-            @variable(m, -5 <= y <= 0)
-            @constraint(m, min(x, y) <= 4)
-            @objective(m, Max, x + y)
-            optimize!(m)
-
-            println(termination_status(m))
-
-            # @test value(x) ≈ 4.0
-            # @test value(y) ≈ 0.0
-        end
-
-        @testset "Both variables mixed sign" begin
-            m = Model(with_optimizer(GLPK.Optimizer))
-            @variable(m, -5 <= x <= 5)
-            @variable(m, -5 <= y <= 5)
-            @constraint(m, min(x, y) <= 4)
-            @objective(m, Max, x + y)
-            optimize!(m)
-
-            println(termination_status(m))
-
-            # @test value(x) ≈ 4.0
-            # @test value(y) ≈ 4.0
+                if value(x) ≈ 4.0
+                    @test value(x) ≈ 4.0
+                    @test value(y) ≈ 5.0
+                    @test value(z) ≈ 5.0
+                elseif value(y) ≈ 4.0
+                    @test value(x) ≈ 5.0
+                    @test value(y) ≈ 4.0
+                    @test value(z) ≈ 5.0
+                else
+                    @test value(x) ≈ 5.0
+                    @test value(y) ≈ 5.0
+                    @test value(z) ≈ 4.0
+                end
+            end
         end
     end
 end
